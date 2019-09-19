16a58252abe2e19c8a38b8b2753932a1c905bd71
rCarter101 <xorn101@ufl.edu>
2019-09-18 23:05:07 -0400

    Update queries.js

--- queries.js
+++ queries.js
@@ -6,7 +6,7 @@ var fs = require('fs'),
     config = require('./config'),
     uri = config.db.uri;
 /* Connect to your database using mongoose - remember to keep your key secret*/
-mongoose.connect(uri, {useNewUrlParser: true});
+mongoose.connect(uri, {useUnifiedTopology: true, useNewUrlParser: true});
 /* Fill out these functions using Mongoose queries*/
 //Check out - https://mongoosejs.com/docs/queries.html
 
@@ -15,8 +15,10 @@ var findLibraryWest = function() {
     Find the document that contains data corresponding to Library West,
     then log it to the console. 
    */
-   var temp = Listing.find({Code: 'LBW'});
-   console.log(temp.toString());
+   var temp = Listing.findOne({ Code: 'LBW' }, function(err, stuff){
+    console.log(stuff);
+   });
+   
 
 };
 var removeCable = function() {
@@ -25,8 +27,9 @@ var removeCable = function() {
     on cable TV. Since we live in the 21st century and most courses are now web based, go ahead
     and remove this listing from your database and log the document to the console. 
    */
-   var temp = Listing.find({Code: 'CABL'});
-   console.log(temp.toString());
+   var temp = Listing.find({Code: 'CABL'}, function(err, stuff){
+       console.log(stuff);
+   });
    Listing.deleteOne({ Code: 'CABL' }, function (err) {});
 };
 var updatePhelpsMemorial = function() {
@@ -35,15 +38,17 @@ var updatePhelpsMemorial = function() {
     log the updated document to the console. 
    */
    Listing.findOneAndUpdate({Code: 'PHL'}, {address: '701 North Broadway, Sleepy Hollow 10591, New York, United States'});
-   var temp = Listing.find({Code: 'PHL'});
-   console.log(temp.toString());
+   var temp = Listing.find({Code: 'PHL'}, function(err, stuff){
+       console.log(stuff);
+   });
 };
 var retrieveAllListings = function() {
   /* 
     Retrieve all listings in the database, and log them to the console. 
    */
-   var temp = Listing.find({});
-   console.log(temp.toString());
+   var temp = Listing.find(function(err, stuff){
+    console.log(stuff);
+   });
 };
 
 findLibraryWest();
